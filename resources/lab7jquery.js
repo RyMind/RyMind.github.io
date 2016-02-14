8450a4054ce6ce33bf17d1efb0730a28e019f148
RyMind <ryanm567@gmail.com>
2016-02-14 17:21:40 -0500

    txt instead of json

--- resources/lab7jquery.js
+++ resources/lab7jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$.getJSON("../resources/lab7jsontemplate.json?format=json", jsonMenu);
+	$.getJSON("../resources/lab7jsontemplate.txt?format=json", jsonMenu);
 	function jsonMenu(menuData){
 		var output = "";	
 		$.each(menuData.menuItem, function(i,item){
