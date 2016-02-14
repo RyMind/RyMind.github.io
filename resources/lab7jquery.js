a95694f6ac909ab2322bb2d8841f1fee76c32676
RyMind <ryanm567@gmail.com>
2016-02-14 15:53:53 -0500

    fix

--- resources/lab7jquery.js
+++ resources/lab7jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$.getJSON("../_data/lab7jsontemplate.json", jsonMenu);
+	$.getJSON("../reasources/lab7jsontemplate.json", jsonMenu);
 	function jsonMenu(menuData){
 		var output = "";	
 		$.each(menuData.menuItem, function(i,item){
